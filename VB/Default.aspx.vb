86d31c9276c2c36f840e2a43e4f54a512ebbaebc
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ VB/Default.aspx.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports DevExpress.XtraCharts
+' ...
+
+Namespace WebChartDataBinding
+    Partial Public Class _Default
+        Inherits System.Web.UI.Page
+
+        Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            WebChartControl1.DataSourceID = "AccessDataSource1"
+            WebChartControl1.SeriesDataMember = "Year"
+            WebChartControl1.SeriesTemplate.ArgumentDataMember = "Region"
+            WebChartControl1.SeriesTemplate.ValueDataMembers.AddRange(New String() {"GSP"})
+            WebChartControl1.SeriesTemplate.View = New SideBySideBarSeriesView()
+            WebChartControl1.DataBind()
+        End Sub
+    End Class
+
+End Namespace
